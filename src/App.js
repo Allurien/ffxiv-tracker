af5016c15a03b93fc75480b7fb131b7f6b420067
Steffany <kielen@gmail.com>
2021-06-06 13:15:39 -0700

    updated app.js to begin prepping for login modal and auth process

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Button, Modal } from 'antd';
 import Header from './components/header';
 import Links from './components/links';
-import Login from './components/login';
 import Splash from './components/splash';
 import Tasks from './components/tasks/tasks';
 import MasterTasks from './components/tasks/masterTasks';
 import './App.css';
 
 function App() {
+    const [visible, setVisible] = React.useState(true);
+    const [token] = React.useState(false);
+    useEffect(() => {
+        setVisible(true);
+    }, []);
     return (
         <div>
             <Header />
             <main>
+                <Modal
+                    title="Login"
+                    visible={visible}
+                    onCancel={()=>setVisible(false)}
+                    footer={null}
+                >
+                    <Button size="large" href={"https://api.beta.ffxiv.anid.dev/login"}>
+                        Login with Discord
+                    </Button>
+                </Modal>
                 <Switch>
                     <Route path="/" exact component={Splash} />
-                    <Route path="/login" exact component={Login} />
                     <Route path="/links" component={Links} />
                     <Route path="/tasks" component={Tasks} />
                     <Route path="/master-tasks" component={MasterTasks} />
